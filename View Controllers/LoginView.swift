c8db4422f4bb13f00d1302ca8d0f811284c242c4
Alexandra Medina <47196412+alexandramedina89@users.noreply.github.com>
2021-10-26 19:15:35 -0500

    Update and rename LoginView.swift to View Controllers/LoginView.swift

--- View Controllers/LoginView.swift
+++ View Controllers/LoginView.swift
@@ -2,7 +2,7 @@
 //  LoginView.swift
 //  IONO
 //
-//  Created by Alexandra Medina on 10/4/21.
+//  Created by Alexandra Medina
 //
 
 import UIKit
@@ -12,41 +12,32 @@ import Alamofire
 
 class LoginView: UIViewController {
     
-    
-    
 
 let URL_USER_LOGIN = URL(string: "http://10.0.1.34/MyWebService/api/login.php")
 
-//the defaultvalues to store user data
+//MARK:  Store default User values
 let defaultValues = UserDefaults.standard
 
-//the connected views
-
-
+//MARK:  Connected views
     @IBOutlet weak var labelMessage: UILabel!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
-
-
- //   @IBAction func loginSubmitFinal(_ sender: UIButton) {
- 
+ //MARK:  User Login Function 
     func login(){
-    
-    //getting the username and password
+
     let parameters: Parameters=[
         "email":emailTextField.text!,
         "password":passwordTextField.text!
     ]
     
-    //making a post request
+    //MARK:  post request
     Alamofire.request(URL_USER_LOGIN!, method: .post, parameters: parameters).responseJSON
         {
             response in
-            //printing response
             print(response)
             
-            //getting the json value from the server
+    //MARK:  Getting the json value from the server
             if let result = response.result.value {
                 let jsonData = result as! NSDictionary
                 
@@ -58,13 +49,11 @@ let defaultValues = UserDefaults.standard
                     
                     //getting user values
                     let userId = user.value(forKey: "id") as! Int
-                   // let userName = user.value(forKey: "l_name") as! String
                     let userEmail = user.value(forKey: "email") as! String
                     let userPhone = user.value(forKey: "phone") as! String
                     
                     //saving user values to defaults
                     self.defaultValues.set(userId, forKey: "userid")
-                   // self.defaultValues.set(userName, forKey: "username")
                     self.defaultValues.set(userEmail, forKey: "useremail")
                     self.defaultValues.set(userPhone, forKey: "userphone")
                     
@@ -94,19 +83,7 @@ let defaultValues = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //hiding the navigation button
-  /*      let backButton = UIBarButtonItem(title: "", style: UIBarButtonItem.Style.plain, target: navigationController, action: nil)
-                navigationItem.leftBarButtonItem = backButton
 
-                // Do any additional setup after loading the view, typically from a nib.
-                
-                //if user is already logged in switching to profile screen
-            if defaultValues.string(forKey: "email") != nil{
-                let profileViewController = self.storyboard?.instantiateViewController(withIdentifier: "firstViewController") as! firstView
-                    self.navigationController?.pushViewController(profileViewController, animated: true)
-
-    }
-  */
 
 }
 
