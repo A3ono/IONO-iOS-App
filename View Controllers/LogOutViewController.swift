bf7042d19161f6d935c01149cbfec76641e0bc06
Alexandra Medina <47196412+alexandramedina89@users.noreply.github.com>
2021-10-26 19:38:55 -0500

    Update and rename ViewController2.swift to View Controllers/LogOutViewController.swift

--- View Controllers/LogOutViewController.swift
+++ View Controllers/LogOutViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController2.swift
+//  LogOutViewController.swift
 //  IONO
 //
 //  Created by Alexandra Medina on 9/5/21.
@@ -7,15 +7,13 @@
 
 import UIKit
 
-class ViewController2: UIViewController {
+class LogOutViewController: UIViewController {
 
     @IBAction func logoutButton(_ sender: Any) {
         
-        //removing values from default
                 UserDefaults.standard.removePersistentDomain(forName: Bundle.main.bundleIdentifier!)
                 UserDefaults.standard.synchronize()
                 
-                //switching to login screen
                 let loginViewController = self.storyboard?.instantiateViewController(withIdentifier: "welcomeLogin") as! ViewController4
                 self.navigationController?.pushViewController(loginViewController, animated: true)
                 self.dismiss(animated: false, completion: nil)
@@ -24,7 +22,6 @@ class ViewController2: UIViewController {
             override func viewDidLoad() {
                 super.viewDidLoad()
                 
-                //hiding back button
                 let backButton = UIBarButtonItem(title: "", style: UIBarButtonItem.Style.plain, target: navigationController, action: nil)
                 navigationItem.leftBarButtonItem = backButton
                 
