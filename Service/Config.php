7a062fa6528d03b96defc039a028b51ff21190a6
Alexandra Medina <47196412+alexandramedina89@users.noreply.github.com>
2021-10-26 19:50:22 -0500

    Update and rename Config.php to Service/Config.php

--- Service/Config.php
+++ Service/Config.php
@@ -3,7 +3,7 @@
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'sample');
+define('DB_NAME', 'iono');
 
 define('USER_CREATED', 0);
 define('USER_ALREADY_EXIST', 1);
