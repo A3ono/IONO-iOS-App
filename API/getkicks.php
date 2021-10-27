afc012a026d12daac796f22542220d3153135357
Alexandra Medina <47196412+alexandramedina89@users.noreply.github.com>
2021-10-26 19:54:44 -0500

    Update and rename getkicks.php to API/getkicks.php

--- API/getkicks.php
+++ API/getkicks.php
@@ -1,22 +1,17 @@
 <?php
-//including the file dboperation
 require_once '../includes/DbOperation.php';
 
-//creating a response array to store data
 $response = array();
 
-//creating a key in the response array to insert values
-//this key will store an array iteself
 $response['kicks'] = array();
 
-//creating object of class DbOperation
 $db = new DbOperation();
 
-//getting the teams using the function we created
 $kicks = $db->getKicks();
 
-//looping through all the teams.
+//looping through kicks
 while($October = $kicks->fetch_assoc()){
+    
     //creating a temporary array
     $temp = array();
 
@@ -29,5 +24,4 @@ while($October = $kicks->fetch_assoc()){
     array_push($response['kicks'],$temp);
 }
 
-//displaying the array in json format
 echo json_encode($response);
